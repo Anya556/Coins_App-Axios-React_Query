9a3ecc0c6342a04dbb3cd96353079f4b3351a4f5
Anna <gstudentseo@gmail.com>
2023-10-08 23:23:39 +0200

    Installed bootstrap

---
+++ crypto-query/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Container from 'react-bootstrap/Container';
+
+function App() {
+  return <Container style={{ marginTop: 30, maxWidth: 600 }}>
+  
+  </Container>;
+}
+
+export default App;
