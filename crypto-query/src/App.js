2667d28581b74b153916aa975ed7d999c99c8594
Anna <gstudentseo@gmail.com>
2023-10-09 23:15:29 +0200

    Data is written as an object

--- crypto-query/src/App.js
+++ crypto-query/src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CoinsTable from './components/CoinsTable';
 import Container from 'react-bootstrap/Container';
 
 function App() {
-  return <Container style={{ marginTop: 30, maxWidth: 600 }}>
-  
-  </Container>;
+  return (
+    <Container style={{ marginTop: 30, maxWidth: 600 }}>
+      <CoinsTable
+        data={[
+          {
+            id: 'bitcoin',
+            icon: 'https://static.coinstats.app/coins/1650455588819.png',
+            name: 'Bitcoin',
+            symbol: 'BTC',
+            rank: 1,
+            price: 23002.0486,
+          },
+        ]}
+      />
+    </Container>
+  );
 }
 
 export default App;
--- crypto-query/src/components/CoinsTable.jsx
+++ crypto-query/src/components/CoinsTable.jsx
@@ -1,3 +1,35 @@
-export const CoinTable = ({ data }) => {
-  
-}
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+
+const CoinsTable = ({ data }) => {
+  return (
+    <Table striped bordered>
+      <thead>
+        <tr>
+          <th>№</th>
+          <th>Title</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((obj) => (
+          <tr>
+            <td>{obj.rank}</td>
+            <td>
+              <img
+                src={obj.icon}
+                width={20}
+                style={{ marginRight: 10 }}
+                alt="Coin"
+              />
+              {obj.name}
+            </td>
+            <td>$ {obj.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default CoinsTable;
